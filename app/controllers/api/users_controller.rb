c052983dbcca00f295c8868f7d2c07bd0936f565
seo9204 <seo9204@gmail.com>
2018-11-14 04:20:53 -0500

    delete photo params in user_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,6 +11,6 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password, :firstname, :lastname, :zipcode, :date, :month, :year, :photo)
+    params.require(:user).permit(:username, :password, :firstname, :lastname, :zipcode, :date, :month, :year)
   end
 end
